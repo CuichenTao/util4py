59a7e20cfe09808e4fbef14a1d0e66d81eeb9405
Taocuichen <taocuichen@wantdata.cn>
2018-04-18 21:32:33 +0800

    modify date utils

--- utils/date_util.py
+++ utils/date_util.py
@@ -8,17 +8,33 @@ import datetime
 日期工具
 """
 
-def get_date():
+def get_current_date():
     """获取今日日期
-    格式: 20171213
+    格式: '20171213'
     """
     return time.strftime("%Y%m%d")
 
-def get_timestamp():
+def get_current_timestamp():
     """获取当前时间戳
     格式: 1524032735404
     """
     return int(round(time.time() * 1000))
+
+def get_current_time():
+    """获取当前时间
+    :return: 时间类型
+    """
+    return datetime.datetime.now()
+
+
+def weekofmonth(date):
+    """当前天是当月中的第几周
+    :param date:
+    :return:第几周
+    """
+    end=int(date.strftime('%W'))
+    start=int(datetime.datetime(date.year,date.month,1).strftime('%W'))
+    return end-start
 
 def formatTime(timestamp):
     """格式化时间戳
@@ -43,10 +59,10 @@ def get_time_before(days=0,hours=0,minutes=0,seconds=0,microseconds=0):
     :return: 获取当前时间戳=> 格式: '2017-12-13 16:32:30'
     """
     res = datetime.datetime.now() - datetime.timedelta(days=days, hours=hours, minutes=minutes, seconds=seconds, microseconds=microseconds)
-    return res.strftime("%Y-%m-%d %H:%M:%S")
+    return res
 
 
 if __name__ == '__main__':
-    print(get_time_before(minutes=10))
+    print(weekofmonth(get_time_before()))
 
 
